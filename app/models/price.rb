fa334f48d265bcb0070dbbc051afc2110c5265d1
devadrsilva <adriano.mssilva1@gmail.com>
2023-11-09 13:22:54 -0300

    área da pousadas feita com sucesso... Ainda falta a validação dos preços

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,17 +1,18 @@
 class Price < ApplicationRecord
   belongs_to :room
+  validates :value, :date_start, :date_end, presence: true
+  validate :range_validate
 
   private
 
-  # def range_validate
-  #   range = Range.new(self.date_start, self.date_end)
-  #   room = self.room
-  #   room.prices.each do |price|
-  #     range2 = Range.new(price.date_start, price.date_end)
-  #     if range.overlap?range2
-  #       errors.add()
-  #     end
-  #   end
-
-  # end
+  def range_validate
+    range = Range.new(self.date_start, self.date_end)
+    room = self.room
+    room.prices.each do |price|
+      range2 = Range.new(price.date_start, price.date_end)
+      if range.overlaps?range2
+        self.errors.add(:date_start, 'Já exite um preço cadastrado para essas datas.')
+      end
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,9 @@
 class Room < ApplicationRecord
   belongs_to :inn
   has_many :prices
+  validates :name, :description, :capacity, :default_price,
+            :bathroom, :balcony, :air_conditioning, :tv, :wardrobe,
+            :safe, :pcd, :dimension, presence: true
 
   enum status: {available: 0, unavailable: 2 }
 end
