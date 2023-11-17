1af3b5154455d2e0598c2d7016e680b388cf2680
devadrsilva <adriano.mssilva1@gmail.com>
2023-11-17 11:24:47 -0300

    Teste de campos obrigatórios da reserva

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
+  describe "Campus vazios não devem passar" do
+    it "teste data de entrada" do
+      # Arrange
+      reserva = Reservation.new(start_date: " ")
+
+      # Act
+      reserva.valid?
+      result = reserva.errors.include?(:start_date)
+
+      # Assert
+      expect(result).to eq true
+
+
+    end
+    it "teste data de saída" do
+      # Arrange
+      reserva = Reservation.new(end_date: " ")
+
+      # Act
+      reserva.valid?
+      result = reserva.errors.include?(:end_date)
+
+      # Assert
+      expect(result).to eq true
+
+
+    end
+    it "teste hóspedes" do
+      # Arrange
+      reserva = Reservation.new(guest_number: " ")
+
+      # Act
+      reserva.valid?
+      result = reserva.errors.include?(:guest_number)
+
+      # Assert
+      expect(result).to eq true
+
+
+    end
+
+
+  end
 
 end
